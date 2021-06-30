312f5803ab7faa7da7a6c4146205c589e2533499
whj <153104389@qq.com>
2021-06-30 16:51:46 +0800

    feat: init vite-react

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import routes from '@/src/router/routes'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
+import moment from 'moment'
+import 'moment/dist/locale/zh-cn'
+moment.locale('zh-cn')
 
 const App = () => {
-  return <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+  return (
+    <ConfigProvider locale={zhCN}>
+      <BrowserRouter>{renderRoutes(routes)}</BrowserRouter>
+    </ConfigProvider>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from '@/store/index'
-import { ConfigProvider } from 'antd'
-import zhCN from 'antd/es/locale/zh_CN'
-import moment from 'moment'
-import 'moment/dist/locale/zh-cn'
-moment.locale('zh-cn')
+
 import App from '@/src/App'
 import 'nprogress/nprogress.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ConfigProvider locale={zhCN}>
-        <App />
-      </ConfigProvider>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
